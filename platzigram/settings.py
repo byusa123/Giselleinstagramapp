196e42a58ebfbfee635f3a852db027da2410806b
Byusa Giselle <69889793+byusa123@users.noreply.github.com>
2021-01-09 11:19:58 +0200

    Update settings.py

--- platzigram/settings.py
+++ platzigram/settings.py
@@ -59,7 +59,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
 
     # Simplified static file serving.
-    https://warehouse.python.org/project/whitenoise/
+    'https://warehouse.python.org/project/whitenoise/',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
